088522eafaf3470859e7a8911e67d5c7ec72a54c
margish288 <margispatel288@gmail.com>
2024-03-12 16:51:36 +0530

    Done : Dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,86 @@
+"use client";
+
+import {
+  ArrowRight,
+  Code2,
+  ImageIcon,
+  MessageSquare,
+  Music2Icon,
+  VideoIcon,
+} from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    href: "/image-generation",
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    href: "/video-generation",
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+  },
+  {
+    label: "Code Generation",
+    icon: Code2,
+    href: "/code-generation",
+    color: "text-yellow-500",
+    bgColor: "bg-yellow-500/10",
+  },
+  {
+    label: "Music Generation",
+    icon: Music2Icon,
+    href: "/music-generation",
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+  },
+];
+
 export default function Dashboard() {
+  const router = useRouter();
+
   return (
     <div>
-      <p>Dashboard page</p>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of PARTH
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Code.ai is a platform that allows you to generate code using PARTH. It
+          uses the power of OpenAI GPT-3 to generate code from natural language.
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("size-8", tool.color)} />
+              </div>
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+            <ArrowRight className="size-5" />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
